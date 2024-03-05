ec34e098a220884bb1c89a20d2ed0b2f11a89e83
Apiraam Selvabaskaran <apiraam96@gmail.com>
2024-03-04 20:38:32 -0500

    added first emoji key

--- src/client/pages/EmojifyPage.tsx
+++ src/client/pages/EmojifyPage.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Kbd } from 'flowbite-react'
 
 const EmojifyPage = () => {
+    const emojis = []
 return (
+    <>
     <h1>Emojify Page</h1>
+    <div>
+        <Kbd>😀</Kbd>
+    </div>
+    </>
     )
 }
 
