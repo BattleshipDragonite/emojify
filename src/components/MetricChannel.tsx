3139cc1b31a21925d7927ab2bbac6388a843b7a9
Daniel Song <81377675+djsong15@users.noreply.github.com>
2024-03-04 22:28:38 -0800

    Fix indentation

--- src/components/MetricChannel.tsx
+++ src/components/MetricChannel.tsx
@@ -13,18 +13,18 @@ const MetricChannel = ({
 
   return (
     <div id={metric} className="channel">
-        <label htmlFor={metric + "-slider"}>{maxEmoji}</label>
-        <input
-          id={metric + "-slider"}
-          type="range"
-          min={min}
-          max={max}
-          defaultValue={defaultValue}
-          step={step}
-          className="slider w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
-        />
-        <label htmlFor={metric + "-slider"}>{minEmoji}</label>
-      </div>
+      <label htmlFor={metric + "-slider"}>{maxEmoji}</label>
+      <input
+        id={metric + "-slider"}
+        type="range"
+        min={min}
+        max={max}
+        defaultValue={defaultValue}
+        step={step}
+        className="slider w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+      />
+      <label htmlFor={metric + "-slider"}>{minEmoji}</label>
+    </div>
   )
 };
 
