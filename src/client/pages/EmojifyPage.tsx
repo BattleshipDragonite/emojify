d312c36bcf98fa50fb048880985ca6feab911785
Daniel Song <81377675+djsong15@users.noreply.github.com>
2024-03-04 20:16:54 -0800

    Fix indentation

--- src/client/pages/EmojifyPage.tsx
+++ src/client/pages/EmojifyPage.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Kbd } from 'flowbite-react'
 
+import MetricsMixer from '../../components/MetricsMixer'
+
 const EmojifyPage = () => {
     const emojis = []
-return (
-    <>
-    <h1>Emojify Page</h1>
-    <div>
-        <Kbd>😀</Kbd>
-    </div>
-    </>
+    return (
+        <>
+            <h1>Emojify Page</h1>
+            <div id='user-interface'>
+                <Kbd>😀</Kbd>
+                <MetricsMixer />
+            </div>
+        </>
     )
 }
 
