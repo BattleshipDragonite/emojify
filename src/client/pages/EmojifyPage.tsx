da51a806a3af44204861f18b22a385c8610ca564
Apiraam Selvabaskaran <apiraam96@gmail.com>
2024-03-05 21:05:58 -0500

    chore: removed old code

--- src/client/pages/EmojifyPage.tsx
+++ src/client/pages/EmojifyPage.tsx
@@ -1,24 +1,12 @@
 import React, {useState} from 'react'
-import { Kbd } from 'flowbite-react'
+import { Button } from 'flowbite-react'
 import {genresMap} from '../../server/utils/emojiDict.ts'
 
 const EmojifyPage = () => {
-    const emojis = ['🪵', '🇳🇬', '🗿', '🧑‍🎤', '🌫️', '']
     const [genre, setGenre] = useState('🎼')
 
     const genres = Object.keys(genresMap)
 
-    const Backspace = (searchWord) => {
-        let backSpacedWord = searchWord.split('')
-        backSpacedWord.pop()
-        backSpacedWord.join()
-        console.log(backSpacedWord)
-        return backSpacedWord
-    }
-
-    const SearchFunction = () => {
-        const emojiRegex = '/^(?:(?:\p{RI}\p{RI}|\p{Emoji}(?:\p{Emoji_Modifier}|\u{FE0F}\u{20E3}?|[\u{E0020}-\u{E007E}]+\u{E007F})?(?:\u{200D}\p{Emoji}(?:\p{Emoji_Modifier}|\u{FE0F}\u{20E3}?|[\u{E0020}-\u{E007E}]+\u{E007F})?)*)|[\u{1f900}-\u{1f9ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}])+$/u';
-    }
 
 return (
     <div className='flex flex-col align-middle'>
