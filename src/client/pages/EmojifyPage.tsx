3b74998829fe5fcb0d11f2bfae4747b1d021dfe1
Apiraam Selvabaskaran <apiraam96@gmail.com>
2024-03-05 21:41:04 -0500

    Added search button

--- src/client/pages/EmojifyPage.tsx
+++ src/client/pages/EmojifyPage.tsx
@@ -1,6 +1,4 @@
 import React, {useState} from 'react'
-import { Button } from 'flowbite-react'
-
 import MetricsMixer from '../../components/MetricsMixer'
 import {genresMap} from '../../server/utils/emojiDict.ts'
 
@@ -52,6 +50,11 @@ return (
 
     </div>
     <MetricsMixer />
+    <button type="submit" className="inline-flex items-center py-2.5 px-3 ms-2 text-sm font-medium text-white bg-purple-700 rounded-lg border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">
+        <svg className="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+        </svg>Search
+    </button>
     </div>
     )
 }
