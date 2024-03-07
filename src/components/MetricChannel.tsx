ab8a9c46ff4550ab715e4d0ef6ab58331e20f605
Daniel Song <81377675+djsong15@users.noreply.github.com>
2024-03-06 18:08:08 -0800

    Add number visual for slider values

--- src/components/MetricChannel.tsx
+++ src/components/MetricChannel.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { MetricChannelProps } from "../client/types";
 
 const MetricChannel = ({
@@ -10,9 +10,16 @@ const MetricChannel = ({
   minEmoji,
   maxEmoji
 }: MetricChannelProps) => {
+  const [sliderVal, setSliderVal] = useState(step >= 1 ? defaultValue : defaultValue.toFixed(2));
+
+  const handleSliderChange = (e): void => {
+    const val: string = e.target?.value;
+    setSliderVal(step >= 1 ? val : parseFloat(val).toFixed(2));
+  }
 
   return (
     <div id={metric} className="metric-channel">
+      <span>{sliderVal}</span>
       <label htmlFor={metric + "-slider"}>{maxEmoji}</label>
       <input
         id={metric + "-slider"}
@@ -22,6 +29,7 @@ const MetricChannel = ({
         defaultValue={defaultValue}
         step={step}
         className="slider w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+        onInput={handleSliderChange}
       />
       <label htmlFor={metric + "-slider"}>{minEmoji}</label>
     </div>
