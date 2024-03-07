f4a1402b407ea9eb47495cec9c077418a6943bdf
Daniel Song <81377675+djsong15@users.noreply.github.com>
2024-03-06 19:15:30 -0800

    Provide visual for aux metrics

--- src/components/AuxChannel.tsx
+++ src/components/AuxChannel.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { AuxChannelProps } from "../client/types";
 
 const AuxChannel = ({
@@ -6,33 +6,37 @@ const AuxChannel = ({
   min,
   max,
   defaultValue,
+  possValues,
+  currMetrics,
+  setMetrics
 }: AuxChannelProps) => {
+  const [auxVal, setAuxVal] = useState<string>(possValues[defaultValue - min]);
+  
+  const handleSliderInput = (e): void => {
+    const val: string = e.target?.value;
+    setAuxVal(possValues[parseInt(val) - min]);
+  }
+  const handleSliderChange = (e): void => {
+    const val: string = e.target?.value;
+    setMetrics({
+      ...currMetrics,
+      [metric]: parseFloat(val)
+    });
+  }
 
   return (
     <div id={metric} className="aux-channel relative mb-6">
-      <label htmlFor={metric + "-slider"}>{metric}</label>
+      <label htmlFor={metric + "-slider"}>{metric}: {auxVal}</label>
       <input
         id={metric + "-slider"}
         type="range"
         min={min}
         max={max}
         defaultValue={defaultValue}
+        onInput={handleSliderInput}
+        onChange={handleSliderChange}
         className="slider w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
       />
-      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-0 -bottom-6">0</span>
-      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-1/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">1</span>
-      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-2/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">2</span>
-      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-3/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">2</span>
-      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-4/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">2</span>
-      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-5/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">2</span>
-      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-6/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">2</span>
-      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-7/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">2</span>
-      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-8/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">2</span>
-      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-9/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">2</span>
-      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-10/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">2</span>
-      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-11/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">2</span>
-      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-12/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">2</span>
-      <span className="text-sm text-gray-500 dark:text-gray-400 absolute end-0 -bottom-6">3</span>
     </div>
   )
 };
