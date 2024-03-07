3b96c3ae47f8c086d7bf42982a9d724c89620b9e
Daniel Song <81377675+djsong15@users.noreply.github.com>
2024-03-06 17:26:37 -0800

    Add placeholder span elements in AuxChannel

--- src/components/AuxChannel.tsx
+++ src/components/AuxChannel.tsx
@@ -9,7 +9,7 @@ const AuxChannel = ({
 }: AuxChannelProps) => {
 
   return (
-    <div id={metric} className="aux-channel">
+    <div id={metric} className="aux-channel relative mb-6">
       <label htmlFor={metric + "-slider"}>{metric}</label>
       <input
         id={metric + "-slider"}
@@ -19,6 +19,20 @@ const AuxChannel = ({
         defaultValue={defaultValue}
         className="slider w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
       />
+      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-0 -bottom-6">0</span>
+      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-1/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">1</span>
+      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-2/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">2</span>
+      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-3/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">2</span>
+      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-4/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">2</span>
+      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-5/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">2</span>
+      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-6/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">2</span>
+      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-7/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">2</span>
+      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-8/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">2</span>
+      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-9/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">2</span>
+      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-10/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">2</span>
+      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-11/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">2</span>
+      <span className="text-sm text-gray-500 dark:text-gray-400 absolute start-12/13 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">2</span>
+      <span className="text-sm text-gray-500 dark:text-gray-400 absolute end-0 -bottom-6">3</span>
     </div>
   )
 };
