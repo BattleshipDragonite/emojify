26ba9fc0c0a8bc04ef2ec0eff702fcfe2ae4036d
Daniel Song <81377675+djsong15@users.noreply.github.com>
2024-03-09 09:09:23 -0800

    Separate bar pixel into its own component. Calculate loudness height

--- src/client/components/VisualizerBar.tsx
+++ src/client/components/VisualizerBar.tsx
@@ -1,23 +1,28 @@
 import React, { useState, useEffect } from "react";
+import VisualizerBarPixel from "./VisualizerBarPixel";
 import { metricsOptionObj } from "../../server/utils/emojiDict";
 
-const VisualizerBar = ({ id, metrics}) => {
+const VisualizerBar = ({ color, metric, metrics}) => {
   const [bar, setBar] = useState<JSX.Element[]>([]);
   useEffect(() => {
-    const height: number = Math.round(metrics.acousticness / metricsOptionObj.acousticness.max * 10) / 10;
-    const color: string = `hsl(0, 100%, ${70 - (height * 30)}%`;
+    // height must account for metrics that can have negative values (i.e. loudness)
+    const height: number = Math.round(
+      Math.abs(metrics[metric]) / (metricsOptionObj[metric].max - metricsOptionObj[metric].min) * 10
+    ) / 10;
+    // const color: string = `hsl(0, 100%, ${70 - (height * 30)}%`;
     console.log(height);
     const divs: JSX.Element[] = [];
     for (let i = 1; i < height * 10; i++) {
-      divs.push(<div className="pixel" style={{
-        backgroundColor: color
-      }}></div>)
+      // divs.push(<div className="pixel" style={{
+      //   backgroundColor: color
+      // }}></div>)
+      divs.push(<VisualizerBarPixel color={color} height={height}/>)
     }
     setBar(divs)
   }, [metrics])
 
   return (
-    <div className="viz-bars" id={id}>
+    <div className="viz-bars">
       {bar}
     </div>
   );
