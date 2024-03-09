3375b6779fbef41fafe7288868e7185d8546e7d8
Daniel Song <81377675+djsong15@users.noreply.github.com>
2024-03-09 09:19:49 -0800

    Render VizBars

--- src/client/components/Display.tsx
+++ src/client/components/Display.tsx
@@ -9,7 +9,15 @@ const Display = ({ genre, metrics }) => {
         Genre: {genre}
       </div>
       <div id="visualizer">
-        <VisualizerBar id="aco-viz" metrics={metrics}/>
+        <VisualizerBar color={0} metric="acousticness" metrics={metrics}/>
+        <VisualizerBar color={13} metric="danceability" metrics={metrics}/>
+        <VisualizerBar color={33} metric="energy" metrics={metrics}/>
+        <VisualizerBar color={52} metric="instrumentalness" metrics={metrics}/>
+        <VisualizerBar color={92} metric="liveness" metrics={metrics}/>
+        <VisualizerBar color={170} metric="speechiness" metrics={metrics}/>
+        <VisualizerBar color={231} metric="valence" metrics={metrics}/>
+        <VisualizerBar color={266} metric="loudness" metrics={metrics}/>
+        <VisualizerBar color={290} metric="tempo" metrics={metrics}/>
       </div>
     </div>
   );
