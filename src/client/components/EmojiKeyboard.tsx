1c01d79118c4f3f97858dae9dfb736f2bf52d9fc
Daniel Song <81377675+djsong15@users.noreply.github.com>
2024-03-08 16:31:45 -0800

    Render keyboard rows using new components

--- src/client/components/EmojiKeyboard.tsx
+++ src/client/components/EmojiKeyboard.tsx
@@ -1,81 +1,19 @@
 import React from "react";
+import EmojiKBDRow from "./EmojiKBDRow.tsx";
 import { genresMap } from "../../server/utils/emojiDict.ts";
 
 
-const EmojiKeyboard = () => {
+const EmojiKeyboard = ({ setGenre }) => {
   const genres = Object.keys(genresMap);
 
   return (
     <div className="keyboard">
-      <div className="flex">
-        {genres.slice(0, 20).map((ele) => {
-          return (
-            <>
-              <div className="keys" onClick={() => setGenre(ele)}>
-                <h1>{ele}</h1>
-              </div>
-            </>
-          );
-        })}
-      </div>
-      <div className="flex">
-        {genres.slice(20, 36).map((ele) => {
-          return (
-            <>
-              <div className="keys" onClick={() => setGenre(ele)}>
-                <h1>{ele}</h1>
-              </div>
-            </>
-          );
-        })}
-      </div>
-      <div className="flex">
-        {genres.slice(36, 55).map((ele) => {
-          return (
-            <>
-              <div className="keys" onClick={() => setGenre(ele)}>
-                <h1>{ele}</h1>
-              </div>
-            </>
-          );
-        })}
-      </div>
-
-      <div className="flex">
-        {genres.slice(55, 75).map((ele) => {
-          return (
-            <>
-              <div className="keys" onClick={() => setGenre(ele)}>
-                <h1>{ele}</h1>
-              </div>
-            </>
-          );
-        })}
-      </div>
-
-      <div className="flex">
-        {genres.slice(75, 90).map((ele) => {
-          return (
-            <>
-              <div className="keys" onClick={() => setGenre(ele)}>
-                <h1>{ele}</h1>
-              </div>
-            </>
-          );
-        })}
-      </div>
-
-      <div className="flex">
-        {genres.slice(90).map((ele) => {
-          return (
-            <>
-              <div className="keys" onClick={() => setGenre(ele)}>
-                <h1>{ele}</h1>
-              </div>
-            </>
-          );
-        })}
-      </div>
+      <EmojiKBDRow start={0} end={20} genres={genres} setGenre={setGenre} />
+      <EmojiKBDRow start={20} end={36} genres={genres} setGenre={setGenre} />      
+      <EmojiKBDRow start={36} end={55} genres={genres} setGenre={setGenre} />
+      <EmojiKBDRow start={55} end={75} genres={genres} setGenre={setGenre} />
+      <EmojiKBDRow start={75} end={90} genres={genres} setGenre={setGenre} />
+      <EmojiKBDRow start={90} genres={genres} setGenre={setGenre} />
     </div>
   )
 }
