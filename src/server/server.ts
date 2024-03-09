a71b9dcc05f0b9756d9e336bb573acbda3f6ad67
Caleb-Kao <kcaleb101@gmail.com>
2024-03-08 23:41:15 -0800

    fix for CORS issue

--- src/server/server.ts
+++ src/server/server.ts
@@ -9,9 +9,17 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 const app = express();
+
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors(), function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use("/", router);
 app.use(errorHandler);
 
