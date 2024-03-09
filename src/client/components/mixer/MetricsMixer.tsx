d2bb93d03e998c0d6b5dfda76943fcb0ddff0d6a
Daniel Song <81377675+djsong15@users.noreply.github.com>
2024-03-08 16:54:56 -0800

    Split AuxMixer into separate component

--- src/client/components/mixer/MetricsMixer.tsx
+++ src/client/components/mixer/MetricsMixer.tsx
@@ -1,8 +1,7 @@
-import React, { useState, useRef } from "react";
+import React from "react";
 import MetricChannel from "./MetricChannel";
-import AuxChannel from "./AuxChannel";
 
-export const MetricsMixer = ({ currMetrics, setMetrics }) => {
+const MetricsMixer = ({ currMetrics, setMetrics }) => {
 
   return (
       <div id="mixer">
@@ -106,54 +105,7 @@ export const MetricsMixer = ({ currMetrics, setMetrics }) => {
           setMetrics={setMetrics}
         />
       </div>
-  )
+    );
   }
 
-export const AuxMixer = ({ currMetrics, setMetrics }) => {
-
-return (
-      <div id="auxiliary">
-        <AuxChannel 
-          metric="key"
-          min={-1}
-          max={11}
-          defaultValue={0}
-          possValues={[
-            'n/a',
-            'C',
-            'C#/D♭',
-            'D',
-            'D#/E♭',
-            'E',
-            'F',
-            'F#/G♭',
-            'G',
-            'G#/A♭',
-            'A',
-            'A#/B♭',
-            'B'
-          ]}
-          currMetrics={currMetrics}
-          setMetrics={setMetrics}
-        />
-        <AuxChannel 
-          metric="mode"
-          min={0}
-          max={1}
-          defaultValue={1}
-          possValues={['minor', 'major']}
-          currMetrics={currMetrics}
-          setMetrics={setMetrics}
-        />
-        <AuxChannel 
-          metric="time_signature"
-          min={3}
-          max={7}
-          defaultValue={4}
-          possValues={['3/4', '4/4', '5/4', '6/4', '7/4']}
-          currMetrics={currMetrics}
-          setMetrics={setMetrics}
-        />
-      </div>
-  );
-}
+  export default MetricsMixer;
