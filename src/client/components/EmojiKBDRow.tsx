9d668b20f92fbabcba491d18d2a1d31f6d03ebd3
Daniel Song <81377675+djsong15@users.noreply.github.com>
2024-03-08 16:36:24 -0800

    Finalize EmojiKBDRow component

--- src/client/components/EmojiKBDRow.tsx
+++ src/client/components/EmojiKBDRow.tsx
@@ -2,23 +2,28 @@ import React from "react";
 
 type EmojiKBDRowProps = {
   start: number,
-  end: number,
-  genres: any,
+  end?: number,
+  genres: string[],
   setGenre: (arg: string) => void
 }
 
-const EmojiKBDRow = ({ start, end, genres, setGenre }) => {
-  <div className="flex">
-    {genres.slice(start, end).map((ele) => {
-      return (
-        <>
+const EmojiKBDRow = ({
+  start,
+  end,
+  genres,
+  setGenre
+}: EmojiKBDRowProps) => {
+  return (
+    <div className="flex">
+      {genres.slice(start, end).map((ele) => {
+        return (
           <div className="keys" onClick={() => setGenre(ele)}>
             <h1>{ele}</h1>
           </div>
-        </>
-      );
-    })}
-  </div>
+        );
+      })}
+    </div>
+  )
 }
 
 export default EmojiKBDRow;
